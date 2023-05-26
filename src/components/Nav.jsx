2a13134a3e37c45a1f8cc275690ffc38e3e25313
theincrediblevee <theincrediblevee@kali.theincrediblevee>
2023-05-26 21:03:20 +0530

    changes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -10,11 +10,11 @@ function Nav(props) {
           {chatWithMentor}
         </div>
         <div className="profile-name">
-          <img className="profile-name-item" src="./src/assets/account-circle.svg" alt="Account circle" />
+          <img className="profile-name-item" src="../assets/account-circle.svg" alt="Account circle" />
           <div className="profile-name-1">
             {profilename}
           </div>
-          <img className="profile-name-item" src="./src/assets/keyboard-arrow-down.svg" alt="Keyboard arrow down" />
+          <img className="profile-name-item" src="../assets/keyboard-arrow-down.svg" alt="Keyboard arrow down" />
         </div>
       </div>
     );
